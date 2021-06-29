cc8cc167bb59c6d7539b816b50576c29ad2833f3
nightthinkerdawnreddy <reraar1.1@gmail.com>
2021-06-29 11:35:23 +0530

    instagram

--- mysqltesting.py
+++ mysqltesting.py
@@ -1,19 +1,5 @@
 import mysql.connector
 import time
-
-#mydb = mysql.connector.connect(
-#  host="localhost",
-#  user="bhaskar",
-#  password="Bhas3434@",
-#  database="djangoone"
-#)
-#mycursor=mydb.cursor()
-#mycursor.execute("select * from testing_urlposting")
-#results=mycursor.fetchall()
-#mycursor.execute("delete form testing_urlposting where urlpost=h%")
-#for i in results:
-#    print(i[1])
-###selenium part starts
 from selenium import webdriver
 driver=webdriver.Chrome(executable_path="./chromedriver")
 driver.get("https://www.instagram.com/accounts/login/")
@@ -28,8 +14,7 @@ time.sleep(180)
 #password=driver.find_element_by_name("password")
 #username.send_keys("dawnreddy1111")
 #the thing is that the files keeps on quering the database of the table of the final for now leave it and continue to the next 
-#password.send_keys("Bhas3434@")
-#driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button').click()
+#password.send_keys("Bhas3434@
 idone=[]
 while True:
   mydb = mysql.connector.connect(
